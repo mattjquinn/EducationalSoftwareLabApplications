48d94bbd01785aba045511394fa85b00fab2edc5
TSAdmin <none@none.com>
2018-07-08 03:43:52 -0400

    Fixing URL conflicts between NT and JM; deleting unneeded templates.

--- jengamtandao/urls.py
+++ jengamtandao/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^mwanafunzi/([0-9]+)/$', views.mwanafunzi, name='mwanafunzi'),
-    url(r'^changamoto/([0-9]+)/([0-9]+)$', views.changamoto, name='changamoto'),
-    url(r'^hongera/([0-9]+)/([0-9]+)$', views.hongera, name='hongera'),
-    url(r'^reset/([0-9]+)/([0-9]+)$', views.reset, name='reset'),
-    url(r'^save_code$', views.save_code, name='save_code'),
-    url(r'^submit_code$', views.submit_code, name='submit_code'),
+    url(r'^$', views.index, name='jm.index'),
+    url(r'^login/$', views.login, name='jm.login'),
+    url(r'^mwanafunzi/([0-9]+)/$', views.mwanafunzi, name='jm.mwanafunzi'),
+    url(r'^changamoto/([0-9]+)/([0-9]+)$', views.changamoto, name='jm.changamoto'),
+    url(r'^hongera/([0-9]+)/([0-9]+)$', views.hongera, name='jm.hongera'),
+    url(r'^reset/([0-9]+)/([0-9]+)$', views.reset, name='jm.reset'),
+    url(r'^save_code$', views.save_code, name='jm.save_code'),
+    url(r'^submit_code$', views.submit_code, name='jm.submit_code'),
 ]
 
