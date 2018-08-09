074776ffbddec723a1a834b6c78c1be4c1954029
TSAdmin <none@none.com>
2018-08-09 00:59:09 -0400

    Initial commit of PyxelDesigner.

---
+++ pyxeldesigner/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='pd.index'),
+#    url(r'^login/$', views.login, name='pd.login'),
+#    url(r'^mwanafunzi/([0-9]+)/$', views.mwanafunzi, name='pd.mwanafunzi'),
+#    url(r'^changamoto/([0-9]+)/([0-9]+)$', views.changamoto, name='pd.changamoto'),
+#    url(r'^hongera/([0-9]+)/([0-9]+)$', views.hongera, name='pd.hongera'),
+#    url(r'^reset/([0-9]+)/([0-9]+)$', views.reset, name='pd.reset'),
+#    url(r'^save_code$', views.save_code, name='pd.save_code'),
+#    url(r'^submit_code$', views.submit_code, name='pd.submit_code'),
+]
+
